f136fc5c9473a4aa3a37627d9b36f6b1c2e2f9c7
yannickKPL <y.cappelle@orange.fr>
2021-02-25 09:01:27 +0100

    mes fichiers

---
+++ caisse yan/Caisse/Classes/CashRegister.cs
@@ -0,0 +1,88 @@
+﻿using Caisse.Tools;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Caisse.Classes
+{
+    //Caisse enregistreuse
+    class CashRegister
+    {
+        List<Product> products;
+        List<Order> orders;
+        private static SqlCommand command;
+        private static SqlDataReader reader;
+
+        public List<Product> Products { get => products; set => products = value; }
+        public List<Order> Orders { get => orders; set => orders = value; }
+
+        public CashRegister()
+        {
+            Products = new List<Product>();
+            orders = new List<Order>();
+            Products = loadProducts();
+        }
+
+        public Product CreateProduct(string title, decimal price, int stock)
+        {
+            //Vérification sur les champs ....
+            Product product = new Product() { Title = title, Price = price, Stock = stock };
+
+            if (product.Save())
+            {
+                Products.Add(product);
+                return product;
+            }
+
+            return null;
+
+
+
+        }
+
+
+        public List<Product> loadProducts()
+        {
+
+            List<Product> products = new List<Product>();
+            //Requete save product
+            string request = "SELECT title,price,stock,id FROM  product";
+            command = new SqlCommand(request, DataBase.Connection);
+
+            DataBase.Connection.Open();
+            reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Product p = new Product() { Title = reader.GetString(0), Price = reader.GetDecimal(1), Stock = reader.GetByte(2),Id=reader.GetInt32(3) };
+                products.Add(p);
+
+
+            }
+            reader.Close();
+            command.Dispose();
+            DataBase.Connection.Close();
+
+            return products;
+
+        }
+
+
+
+
+
+
+        public bool AddOrder(Order order)
+        {
+            Orders.Add(order);
+            return true;
+        }
+
+        public Product SearchProductById(int id)
+        {
+            //Effectuer une recherche de produit avec une expression lambda et la méthode find des listes
+            return Products.Find(p => p.Id == id);
+        }
+    }
+}
---
+++ caisse yan/Caisse/Classes/Product.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using Caisse.Tools;
+
+
+
+namespace Caisse.Classes
+{
+    class Product
+    {
+        private int id;
+        private string title;
+        private decimal price;
+        private int stock;
+        private static int count = 0;
+        private static SqlCommand command;
+        private static SqlDataReader reader;
+
+        public int Id { get => id; set => id = value; }
+        public string Title { get => title; set => title = value; }
+        public decimal Price { get => price; set => price = value; }
+        public int Stock { get => stock; set => stock = value; }
+
+
+        public Product()
+        {
+            id = ++count;
+        }
+
+        public bool Save()
+        {
+            //Requete save product
+            string request = "INSERT INTO product (title, price, stock) OUTPUT INSERTED.ID values (@title, @price, @stock)";
+            command = new SqlCommand(request, DataBase.Connection);
+            command.Parameters.Add(new SqlParameter("@title", Title));
+            command.Parameters.Add(new SqlParameter("@price", Price));
+            command.Parameters.Add(new SqlParameter("@stock", Stock));
+            DataBase.Connection.Open();
+            Id = (int)command.ExecuteScalar();
+            command.Dispose();
+            DataBase.Connection.Close();
+            return Id > 0;
+
+
+        }
+
+
+        public bool UpdateStock()
+        {
+            string request = "UPDATE product SET stock= @stock WHERE id=@id";
+
+            command = new SqlCommand(request, DataBase.Connection);
+            command.Parameters.Add(new SqlParameter("@stock", Stock));
+            command.Parameters.Add(new SqlParameter("@id", Id));
+
+            DataBase.Connection.Open();
+            int nb=command.ExecuteNonQuery();
+            command.Dispose();
+            DataBase.Connection.Close();
+            return nb>0;
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public override string ToString()
+        {
+            return $"Id : {Id}, Titre : {Title}, Prix : {Price}";
+        }
+    }
+}
